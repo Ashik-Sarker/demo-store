c199e2e75ed9d9861cd318659064993899ebbe91
Md.Abu Tahir Sarker <msarker141151@bscse.uiu.ac.bd>
2023-01-08 18:15:16 +0600

    get data

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,7 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
-const store = () => {
-    // fetch('')
+const Store = () => {
+    const [products, setProducts] = useState([]);
+
+    const username = 'ck_35f64c79ebe2cfd6979b6f81c103ff01135ae1b8'
+    const password = 'cs_1dd3842d9bdc656ace99007faef0bb09a4d34400'
+    const requestOptions = {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Basic ' + btoa(username + ":" + password)
+        }
+    };
+
+    useEffect(() => {
+        fetch('https://demostore.mirailit.com/wp-json/wc/v3/products', requestOptions)
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, []);
     
     return (
         <div>
@@ -10,4 +25,4 @@ const store = () => {
     );
 };
 
-export default store;
+export default Store;
