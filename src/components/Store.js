9f9d89e2dbc5e0a7952333f07ab75f75cd5a83aa
Md.Abu Tahir Sarker <msarker141151@bscse.uiu.ac.bd>
2023-01-08 15:48:20 +0600

    first commit

---
+++ src/components/Store.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+
+const store = () => {
+    // fetch('')
+    
+    return (
+        <div>
+            <h1>This is store</h1>
+        </div>
+    );
+};
+
+export default store;
